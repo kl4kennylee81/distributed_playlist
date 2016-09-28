d594b4071e501f2170f8bc510051403963b0575b
Hong Jeon <hongj77@gmail.com>
2016-09-28 17:30:35 -0400

    exit wasn't working properly on test server

--- submission/src/start_node.py
+++ submission/src/start_node.py
@@ -5,10 +5,7 @@ from threading import Thread, Lock
 from socket import SOCK_STREAM, socket, AF_INET
 
 
-
-def main():
-
-
+def main(): pass
 
 if __name__ == "__main__":
 	main()
--- submission/src/test_server.py
+++ submission/src/test_server.py
@@ -5,37 +5,48 @@ from SocketServer import ThreadingMixIn
 # Multithreaded Python server : TCP Server Socket Thread Pool
 class ClientThread(Thread): 
  
-    def __init__(self,ip,port): 
-        Thread.__init__(self) 
-        self.ip = ip 
-        self.port = port 
-        print "[+] New server socket thread started for " + ip + ":" + str(port) 
+    def __init__(self, ip, port, conn): 
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.conn = conn
+        print "[+] New server socket thread started for " + ip + ":" + str(port)
  
     def run(self): 
-        while True : 
-            data = conn.recv(2048) 
+        while True: 
+            data = self.conn.recv(2048) 
+
+            # client wants to end connection
+            if data == 'exit':
+                break
+
             print "Server received data:", data
-            MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
+            MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit: ")
+            self.conn.send(MESSAGE) # echo 
+
+            # I want to end connection with client
             if MESSAGE == 'exit':
                 break
-            conn.send(MESSAGE)  # echo 
 
+        print "ending connection with {}:{}".format(ip,port)
 
 if __name__ == "__main__":
     # Multithreaded Python server : TCP Server Socket Program Stub
-    TCP_IP = '0.0.0.0' 
+    TCP_IP = 'localhost' 
     TCP_PORT = 2004 
      
     tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-    tcpServer.bind((TCP_IP, TCP_PORT)) 
-    threads = [] 
+    tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # prevents addr already in use problem
+    tcpServer.bind((TCP_IP, TCP_PORT))
+    threads = []
      
-    while True: 
+    while True:
+        print "Server is listening on {}:{}...".format(TCP_IP, TCP_PORT)
+        print "There are currently {} connections active".format(len(threads))
         tcpServer.listen(4) 
         print "Multithreaded Python server : Waiting for connections from TCP clients..." 
-        (conn, (ip,port)) = tcpServer.accept() 
-        newthread = ClientThread(ip,port) 
+        (conn, (ip,port)) = tcpServer.accept()
+        newthread = ClientThread(ip,port, conn)
         newthread.start() 
         threads.append(newthread) 
      
