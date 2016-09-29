2ba749fd8a592121312ac9f7e80194889468c02f
Hong Jeon <hongj77@gmail.com>
2016-09-28 22:45:06 -0400

    everything up to the start command works. participants know about each other and each participant listens for possible requests from master.py also

--- submission/src/servers/base_server.py
+++ submission/src/servers/base_server.py
@@ -5,107 +5,108 @@ from threading import Thread, Lock
 import socket
 from socket import SOCK_STREAM, AF_INET
 
-ADDRESS = "localhost"
+
+BUFFER_SIZE = 1024
 START_PORT = 20000
-TCP_IP = '0.0.0.0' # this lets the os handle the selection of your ip
-
-# """
-# Handles Incoming connection on the internal facing socket
-# That will come from other participant processes. This will create
-# a ClientHandler Thread to then service the requests from the
-# participant process.
-
-# This is the server socket that newly started Partcipant processes will
-# attempt to connect to.
-# """
-# class ServerConnectionHandler(Thread):
-
-#   def __init__(free_port_no,pid,server):
-#       Thread.__init__(self)
-#       self.server = server
-#       self.pid = pid
-#       self.internal_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-#       self.internal_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-      
-#       self.internal_server.bind((TCP_IP, free_port_no))
-
-#   def run(self):
-#       while(True):
-#           self.internal_server.listen()
-#           (conn, (ip,port)) = tcpServer.accept() 
-#           print("Pid {}: Connection Accepted".format(pid))
-          
-#           # is there a synchronized decorator?
-#           server.other_procs_lock.acquire()
-
-#           newClientThread = ClientHandler(conn)
-#           server.other_procs.append(newClientThread)
-#           newClientthread.start() 
-
-#           server.other_procs_lock.release() 
+ADDRESS = "localhost"
 
 
-# """
-# The master client is a special client thread that services
-# requests from the master client
-# ex. the application requests
-# """
-# class MasterClientHandler(Thread):
+class MasterClientHandler(Thread):
+  """
+  A special client thread that services requests from the master client 
+  ex. the application requests
+  """
 
-#   def __init__(self,master_sock):
-#       Thread.__init__(self)
-#       self.master_conn = master_sock
+  def __init__(self, master_conn):
+    Thread.__init__(self)
+    self.master_conn = master_conn
+
+  def run(self):
+    while(True):
+      # TODO: logic for handling application requests
+      data = self.master_conn.recv(BUFFER_SIZE)
+      print "got message from master client: {}".format(data)
 
 
-#   def run(self):
-#       while(True):
-#           # handle requests from the master node
+class ClientConnectionHandler(Thread):
+  """
+  Internal server process has an associated client handler thread to handle the 
+  request from other internal servers 
+  ex. the votes/decisions/precommits etc.
+  """
 
-# """
-# Internal server process has an associated client handler thread
-# to handle the request from other internal servers
-# ex. the votes/decisions/precommits etc.
-# """
-# class ClientHandler(Thread):
-#   def __init__(self, index, address, port,server):
-#       Thread.__init__(self)
-#       self.index = index
-#       self.server = server
-#       self.sock = socket(AF_INET, SOCK_STREAM)
-#       self.sock.create_connect((address, port))
-#       self.valid = True
+  def __init__(self):
+    Thread.__init__(self)
 
-#   def __init__(self,index,conn,server):
-#       Thread.__init__(self)
-#       self.index = index
-#       self.server = server
-#       self.sock = conn
-#       self.valid = True
+  @classmethod
+  def fromConnection(cls, conn, pid):
+    result = cls()
+    result.conn = conn
+    result.valid = True
+    result.pid = pid
+    return result
 
-#   def run(self):
-#     pass
-#   # while self.valid:
-#   #     try:
-#   #         # TODO This is where the 3 phase commit will live
-#   #         # this is where the 3 phase commit will be implemented
-#   #     except:
-#   #         # print sys.exc_info()
-#   #         self.valid = False
-#   #         # ERROR HANDLING for WHEN Client Connection CRASHES
-#   #         # del threads[self.index]
-#   #         # self.sock.close()
-#   #         break
+  @classmethod
+  def fromAddress(cls, address, port, pid):
+    result = cls()
+    result.conn = socket.socket(AF_INET, SOCK_STREAM) 
+    result.conn.connect((address, port))
+    result.valid = True
+    result.pid = pid
+    return result
 
-#   def send(self, s):
-#       if self.valid:
-#           self.sock.send(str(s) + '\n')
+  def run(self):
+    print "[+] New ClientConnectionHandler"
+    while True:
+      # TODO: 3 phase commit logic
+      break 
 
-#   def close(self):
-#       try:
-#           self.valid = False
-#           self.sock.close()
-#       except:
-#           pass
+  def send(self, s):
+      if self.valid:
+          self.conn.send(str(s) + '\n')
+
+  def close(self):
+      try:
+          self.valid = False
+          self.conn.close()
+      except:
+          pass
+
+
+class ServerConnectionHandler(Thread):
+  """
+  Handles Incoming connection on the internal facing socket
+  That will come from other participant processes. This will create
+  a ClientConnectionHandler Thread to then service the requests from the
+  participant process.
+
+  This is the server socket that newly started partcipant processes will
+  attempt to connect to.
+  """
+
+  def __init__(self, free_port_no, pid, other_procs, other_procs_lock):
+    Thread.__init__(self)
+    self.pid = pid
+    self.other_procs = other_procs
+    self.other_procs_lock = other_procs_lock
+
+    # this is what all the other participants connect to
+    self.internal_server = socket.socket(AF_INET, SOCK_STREAM) 
+    self.internal_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.internal_server.bind((ADDRESS, free_port_no))
+    print "Initialized server socket at port {}".format(free_port_no)
+
+  def run(self):
+    while True:
+      self.internal_server.listen(0)
+      (conn, (ip,port)) = self.internal_server.accept()
+      print("Pid {}: Connection Accepted".format(self.pid))
+
+      with self.other_procs_lock:
+        new_client_thread = ClientConnectionHandler.fromConnection(conn)
+        self.other_procs.append(new_client_thread)
+        new_client_thread.start()
+
 
 class Server:
   """
@@ -114,9 +115,9 @@ class Server:
   process is alive. To then connect to.
 
   The server is passed onto all the handler threads because it essentially acts
-  as a container for the "global state" of the processes server. Each of the handler
-  needs to reference the server "Global" to get the current state of this participant
-  and to interact and work collectively
+  as a container for the "global state" of the processes server. Each of the 
+  handler needs to reference the server "Global" to get the current state of
+  this participant and to interact and work collectively
   """
 
   """
@@ -134,19 +135,9 @@ class Server:
   """
   
   def __init__(self, pid, n, port):
-    """
-    pid:the unique_id for the server 
-    (that will be used to lookup the transaction log during recovery.)
-
-    n: the number of server processes currently started
-
-    port: the port the master client wants us to listen on in which
-    the master client then connects to after we spawn
-    """
     self.pid = pid
-    self.other_procs = [None for i in xrange(n-1)]
+    self.other_procs = [None for i in range(self.pid)]
     self.other_procs_lock = Lock()
-    # TODO: need to initialize condition variables for the logic parts
 
     self.master_server = socket.socket(AF_INET, SOCK_STREAM) 
     self.master_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -154,20 +145,28 @@ class Server:
 
     # wait synchronously for the master to connect
     self.master_server.listen(0)
-    (master_conn, (ip,client_port)) = self.master_server.accept()
+    (master_conn, (ip, master_client_port)) = self.master_server.accept()
     self.master_thread = MasterClientHandler(master_conn)
     self.master_thread.start()
 
-    # free_port_no = START_PORT + n
-    # self.internal_server = ServerConnectionHandler(free_port_no)
+    # this is the socket that 
+    free_port_no = START_PORT + self.pid
+    self.internal_server = ServerConnectionHandler(free_port_no, 
+                                                    self.pid, 
+                                                    self.other_procs, 
+                                                    self.other_procs_lock)
+    self.internal_server.start()
+    print "Starting server connection handler"
 
-    # for i in range(n-1):
-    #   port_to_connect = START_PORT + i
-    #   try:
-    #     # start up the threads for communicating with all the other participants
-    #     cur_handler = ClientHandler(i,ADDRESS,port_to_connect)
-    #     self.other_procs[i] = cur_handler
-    #     self.other_procs[i].start()
-    #   except:
-    #     print("Error connecting to port:{}".format(port_to_connect))
+    for i in range(self.pid):
+      try:
+        port_to_connect = START_PORT + i
+        cur_handler = ClientConnectionHandler.fromAddress(ADDRESS, 
+                                                          port_to_connect)
+        with self.other_procs_lock:
+          self.other_procs[i] = cur_handler
+          self.other_procs[i].start()
+      except:
+        print sys.exc_info()
+        print("Error connecting to port: {}".format(port_to_connect))
 
